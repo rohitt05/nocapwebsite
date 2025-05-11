dfb919ea091c7fb68298baf40ddbe2349ee433d0
rohitt05 <rohitkattimani172@gmail.com>
2025-05-11 11:59:44 +0530

    updated the error file

---
+++ src/app/error/page.tsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+    return (
+        <div className="p-4">
+            <h1 className="text-xl font-bold">Something went wrong</h1>
+            <p>Please try again later.</p>
+        </div>
+    );
+}
